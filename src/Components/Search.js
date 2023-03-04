902f2a6f034f9ec78f87fbcaf90793970d979d27
Dubey Ravi Vinod <dubeyravivinod@gmail.com>
2023-03-04 19:45:50 +0530

    movie app

---
+++ src/Components/Search.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Fetch from './Fetch';
+import './styles.css';
+
+const Search = () => {
+
+    const [search, setSearch] = useState("");
+
+    const inputHandler = (event) => {
+        event.preventDefault();
+        setSearch(event.target.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        console.log("Clicked");
+    }
+
+    return (
+        <div className='FetchAPIDiv'>
+            <h1 className='hooked'>HOOKED</h1>
+            <input className='inputField' type="text" onChange={inputHandler}/>
+            <button className='SubmitBtn' type='submit' onClick={onSubmitHandler}>SEARCH</button>
+            <p className='para'>Sharing your few favoriate movies</p>
+
+            <Fetch fetchInput={search} />
+        </div>
+    );
+
+}
+
+export default Search;
